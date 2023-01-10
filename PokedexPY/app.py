6212cf91c03aea1d2bdfe0a11225594cd782f682
HenriSousa <Henrisousa7@hotmail.com>
2023-01-10 20:30:59 -0300

    Update app
    Adicionado o fundo da pokedex

--- PokedexPY/app.py
+++ PokedexPY/app.py
@@ -1,11 +1,12 @@
 import pygame
+
 pygame.init()
 
 # Define the size of the screen (width, height).
-size = (700, 500)
+size = (950, 1000)
 screen = pygame.display.set_mode(size)
 
-pygame.display.set_caption("My Game")
+pygame.display.set_caption("Pokedex Pernambucana")
 
 # Loop until the user clicks the close button.
 done = False
@@ -19,13 +20,21 @@ while not done:
         if event.type == pygame.QUIT:
             done = True
 
+    pokedex = pygame.image.load("img/pokedex.png")
+    pokedexP = pokedex.get_rect()
+    screen.blit(pokedex, pokedexP)
+
+    #bulbasaur = pygame.image.load("img/gifPokemons/bulbasaur.gif")
+    #bulbasaurB = bulbasaur.get_rect()
+    #screen.blit(bulbasaur, bulbasaurB)
+
     # --- Game logic should go here
 
     # --- Drawing code should go here
 
     # First, clear the screen to white. Don't put other drawing commands
     # above this, or they will be erased with this command.
-    screen.fill((255, 255, 255))
+    #screen.fill((255, 255, 255))
 
     # --- Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
