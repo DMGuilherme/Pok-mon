a6eda9aedf62e20230dddd82df963d337aad355d
guilherme <guilherme.sunday@gmail.com>
2023-01-12 21:22:56 -0300

    pokedex 0.1

--- PokedexPY/app.py
+++ PokedexPY/app.py
@@ -1,7 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import mysql.connector
+from mysql.connector import Error
 
 app = Flask(__name__, static_folder='static')
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+con = mysql.connector.connect(
+    host='containers-us-west-110.railway.app',
+    port='7415',
+    database='railway',
+    user = 'root',
+    password = 'rvDfUENmgLTJlaqXODGf'
+    )
+
+@app.route('/info')
+def info():
+    cursor = con.cursor()
+    cursor.execute('SELECT * FROM Pokemon where id = 1')
+    result = cursor.fetchall()
+    return jsonify(result)
+
