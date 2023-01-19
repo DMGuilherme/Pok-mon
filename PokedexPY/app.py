d0ccbb0f0d4667c7859d56b44b3100ad34b0b1f8
HenriSousa <Henrisousa7@hotmail.com>
2023-01-19 10:54:09 -0300

    Update 0.5.1
    Função do botão agora passa as imagens de acordo com a ID.

--- PokedexPY/app.py
+++ PokedexPY/app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, jsonify, redirect, url_for
 import mysql.connector
 
 app = Flask(__name__, static_folder='static')
+id = 0
 
 con = mysql.connector.connect(
     host='containers-us-west-110.railway.app',
@@ -14,7 +15,8 @@ con = mysql.connector.connect(
     )
 @app.route('/')
 def index():
-    return render_template('index.html')
+    global id
+    return render_template('index.html', id = id)
 @app.route('/<id>', methods=['GET','POST'])
 def infoPokemons(id):
     cursor = con.cursor()
@@ -22,6 +24,7 @@ def infoPokemons(id):
     result = cursor.fetchall()
     pokemons = json.loads(jsonify(result).get_data())
     [(id_, nome_, tipo_, categoria_, habilidade_, peso_, altura_, fraqueza_, descricao_)] = pokemons
+    id = id_
     nome = nome_
     tipo = tipo_
     categoria = categoria_
@@ -30,23 +33,24 @@ def infoPokemons(id):
     altura = altura_
     fraqueza = fraqueza_
     descricao = descricao_
-    return render_template('index.html', nome = nome,
+    return render_template('index.html', id = id, nome = nome,
                            tipo = tipo, categoria = categoria,
                            habilidade = habilidade, peso = peso,
                            altura = altura, fraqueza = fraqueza,
                            descricao = descricao)
 
-id = 0
 @app.route('/increment', methods=['GET','POST'])
 def increment():
     global id
     id += 1
-    return redirect(url_for('infoPokemons', id=id))
+    return redirect(url_for('infoPokemons', id = id))
 @app.route('/decrease', methods=['GET', 'POST'])
 def decrease():
     global id
-    id -= 1
-    return redirect(url_for('infoPokemons', id=id))
+    if id > 1:
+        id -= 1
+    return redirect(url_for('infoPokemons', id = id))
+
 
 app.run(debug=True)
 #[[1,"Bulbasaur","Grass,Poison","Seed","Overgrow",6.9,0.7,"Fire,Psychic,Flying,Ice",""]]
